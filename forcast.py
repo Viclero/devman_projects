02f58c919eae4508c7c0dc487e396c75e27fa36e
unknown <mr.kvass@list.ru>
2023-01-24 22:55:18 +0300

    commit 2

--- forcast.py
+++ forcast.py
@@ -4,9 +4,8 @@ import urllib
 def show_forcast():
     city_name = ['Лондон', 'аэропорт Шереметьево', 'Череповец']
     setings = {'M':'', 'n':'', 'q':'', 'T':'', 'u':'', 'lang': 'ru'}
-    params = urllib.parse.urlencode(setings, safe='?&')
     for city in city_name:
       url = 'https://wttr.in/'+city
-      response = requests.get(url, params=params)
+      response = requests.get(url, params=setings)
       response.raise_for_status()
       print(response.text)
